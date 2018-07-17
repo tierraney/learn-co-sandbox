4d73ff544e481e2cb912c9f5f0430ec8e5880fbb
tierraney <tierraney@gmail.com>
2018-07-17 14:25:30 +0000

    automatically backed up by learn

---
+++ cardi.rb
@@ -0,0 +1,8 @@
+def cardi
+  she_says = 1
+  while she_says <= 1000
+  puts "OKURRRRRRR, EEEOOOWWW!!"
+  she_says += 1
+  end
+end
+puts cardi
---
+++ practice.rb
@@ -0,0 +1,6 @@
+3 > 4 
+11 < 10
+23 <= 23
+“Vlad” == “Logic”
+“Vlad” + “Logic” == “VladLogic”
+“Vlad” != “Logic”
